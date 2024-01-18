af4bb2e7b79e74078716c3533d128e173d3cf267
Abinash Patra <36896010+Stark7751@users.noreply.github.com>
2024-01-18 15:48:46 +0530

    problem1

---
+++ Two Sum/twoSum.cs
@@ -0,0 +1,19 @@
+//O(n^2) method
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        List<int> result = new List<int> ();
+        for(int i = 0; i < nums.Length ; i++)
+        {
+            for(int j = i+1; j < nums.Length ; j++ )
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    result.Add(i);
+                    result.Add(j);
+                }
+            }
+        }
+        return result.ToArray();
+    }
+}
